ec135ad592b415e4484f345e1b10aa2413e3ce22
Will <will.109@kakaopaycorp.com>
2020-04-06 08:59:47 +0900

    single number 문제 풀이

--- src/main/kotlin/io/icednut/leetcode/exercise/singlenumber/Solution.kt
+++ src/main/kotlin/io/icednut/leetcode/exercise/singlenumber/Solution.kt
@@ -7,3 +7,13 @@ class Solution {
         .entries
         .find { it.value == 1 }?.key ?: 0
 }
+
+class Solution2 {
+    fun singleNumber(nums: IntArray): Int {
+        var result = 0
+        for (x in nums) {
+            result = result xor x
+        }
+        return result
+    }
+}
--- src/test/kotlin/io/icednut/leetcode/exercise/singlenumber/SolutionTest.kt
+++ src/test/kotlin/io/icednut/leetcode/exercise/singlenumber/SolutionTest.kt
@@ -16,4 +16,16 @@ internal class SolutionTest {
         val solution = Solution()
         assertEquals(4, solution.singleNumber(intArrayOf(4,1,2,1,2)))
     }
+
+    @Test
+    fun `싱글 넘버를 구할 수 있어야 한다3`() {
+        val solution = Solution2()
+        assertEquals(1, solution.singleNumber(intArrayOf(2, 2, 1)))
+    }
+
+    @Test
+    fun `싱글 넘버를 구할 수 있어야 한다4`() {
+        val solution = Solution2()
+        assertEquals(4, solution.singleNumber(intArrayOf(4,1,2,1,2)))
+    }
 }
