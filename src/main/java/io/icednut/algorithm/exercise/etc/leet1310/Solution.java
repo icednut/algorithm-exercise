d197abcac5f383dc6453bcb52b4a3a6682a9f60c
icednut <wglee21g@gmail.com>
2024-09-13 10:36:30 +0900

    solve leetcode 1310

---
+++ src/main/java/io/icednut/algorithm/exercise/etc/leet1310/Solution.java
@@ -0,0 +1,43 @@
+package io.icednut.algorithm.exercise.etc.leet1310;
+
+class Solution {
+
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        var xorArr = new int[queries.length];
+
+        for (var i = 0; i < queries.length; i++) {
+            var query = queries[i];
+            var start = query[0];
+            var end = query[1];
+
+            int result = arr[start];
+
+            for (var j = start + 1; j <= end; j++) {
+                int target = arr[j];
+                result = result ^ target;
+            }
+            xorArr[i] = result;
+        }
+        return xorArr;
+    }
+
+    public int[] xorQueries2(int[] arr, int[][] queries) {
+        int n = arr.length;
+        int[] nums = new int[n];
+        int[] res = new int[queries.length];
+        nums[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            nums[i] = nums[i - 1] ^ arr[i];
+        }
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            if (a == 0) {
+                res[i] = nums[b];
+            } else {
+                res[i] = nums[b] ^ nums[a - 1];
+            }
+        }
+        return res;
+    }
+}
---
+++ src/test/java/io/icednut/algorithm/exercise/etc/leet1310/SolutionTest.java
@@ -0,0 +1,68 @@
+package io.icednut.algorithm.exercise.etc.leet1310;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class SolutionTest {
+
+    @Test
+    void test01() {
+        var solution = new Solution();
+
+        var output = solution.xorQueries2(
+                new int[]{1, 3, 4, 8},
+                new int[][]{
+                        new int[]{0, 1},
+                        new int[]{1, 2},
+                        new int[]{0, 3},
+                        new int[]{3, 3},
+                }
+        );
+
+        assertArrayEquals(
+                new int[]{2, 7, 14, 8},
+                output
+        );
+    }
+
+    @Test
+    void test02() {
+        var solution = new Solution();
+
+        var output = solution.xorQueries2(
+                new int[]{4, 8, 2, 10},
+                new int[][]{
+                        new int[]{2, 3},
+                        new int[]{1, 3},
+                        new int[]{0, 0},
+                        new int[]{0, 3},
+                }
+        );
+
+        assertArrayEquals(
+                new int[]{8, 0, 4, 4},
+                output
+        );
+    }
+
+    @Test
+    void test03() {
+        var solution = new Solution();
+
+        var output = solution.xorQueries2(
+                new int[]{16},
+                new int[][]{
+                        new int[]{0, 0},
+                        new int[]{0, 0},
+                        new int[]{0, 0},
+                        new int[]{0, 0},
+                }
+        );
+
+        assertArrayEquals(
+                new int[]{16, 16, 16, 16},
+                output
+        );
+    }
+}
